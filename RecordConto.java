87be397c55591def9a96a7359570d70119b9fa2b
CapVinkus <83774628+CapVinkus@users.noreply.github.com>
2021-06-18 11:49:58 +0200

    Create RecordConto.java

---
+++ RecordConto.java
@@ -0,0 +1,22 @@
+package it.gepo.engine.model;
+
+public class RecordConto {
+	
+	
+	private String conto;
+	private String agente;
+	
+	public String getConto() {
+		return conto;
+	}
+	public void setConto(String conto) {
+		this.conto = conto;
+	}
+	public String getAgente() {
+		return agente;
+	}
+	public void setAgente(String agente) {
+		this.agente = agente;
+	}
+
+}
